2faf29f25906e95d5ca98a9b081da8372a01e9a4
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2021-02-16 20:16:46 +0530

    feat: added photo grid component

---
+++ src/components/PhotoGrid/PhotoGrid.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1em;
+`;
+
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+`;
+
+const PhotoGrid = ({ images }) => {
+  return (
+    <Container>
+      {images.map((image) => (
+        <Image src={image.src} alt={image.view} key={image.view} />
+      ))}
+    </Container>
+  );
+};
+
+export default PhotoGrid;
