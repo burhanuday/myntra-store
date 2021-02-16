25636ceae788aff40cfeaa7afbdda18e640b9353
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2021-02-16 16:33:36 +0530

    feat: add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.header`
+  padding: 1em 3em;
+  box-shadow: 0px 0px 2px 0px #aaa;
+`;
+
+const Header = () => {
+  return <Container>Myntra</Container>;
+};
+
+export default Header;
--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import { Route, Switch, Redirect } from "react-router-dom";
 
+import Header from "../../components/Header/Header";
 import Home from "../Home/Home";
 import ProductDetail from "../ProductDetail/ProductDetail";
 
 const Layout = () => {
   return (
     <main>
-      {/* <Header /> */}
+      <Header />
       <Switch>
         <Route path="/:id" component={ProductDetail} />
         <Route exact path="/" component={Home} />
