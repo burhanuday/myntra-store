c81fdebec2d71f06eb2a47ceb19ae7e3e805e83e
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2021-02-17 15:29:33 +0530

    chore: refactor wishlist to bag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Layout from "./containers/Layout/Layout";
+
 import { FiltersProvider } from "./contexts/filters-context";
+import { BagProvider } from "./contexts/bag-modal-context";
 
 import "./App.css";
 
 function App() {
   return (
-    <FiltersProvider>
-      <Layout />
-    </FiltersProvider>
+    <BagProvider>
+      <FiltersProvider>
+        <Layout />
+      </FiltersProvider>
+    </BagProvider>
   );
 }
 
